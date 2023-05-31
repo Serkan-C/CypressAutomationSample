9e394408944a4f36d133e46c48aa3d6088be27b6
serkan <serkancosturde@gmail.com>
2023-05-31 13:59:03 +0200

    add: first commit

---
+++ cypress/e2e/testStructure.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('Context: my First tests',()=>{
+ before(()=>{
+//runs once before all test cases in this describe block, like beforeclass in TestNG
+
+ })
+beforeEach(()=>{
+//run before each test case, beforeMethod in TestNG
+
+})
+afterEach(()=>{
+    //run after each test case, afterMethod in TestNG
+    
+    })
+after(()=>{
+    //runs once after all test cases finished in this describe block, like afterClass in TestNG
+    
+     })
+
+it('Opening a web application',()=>{
+    cy.visit('/registration_form');
+    
+    cy.get('.nav-link').click();
+})
+
+})
